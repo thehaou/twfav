7fc3610a6be252d4a28ef38a9679a5a2e6538e53
thehaou <ludibrumsea@gmail.com>
2020-11-15 11:59:15 -0500

    Add Mac support (2)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-import urllib.request
 import datetime
-from win32_setctime import setctime
+import json
+import os
+import urllib.request  # python3 only
+# from win32_setctime import setctime  # Uncomment for Windows; python3.5+; mkvirtualenv --> pip install win32-setctime
 
 
 def load_json_file():
@@ -11,6 +12,12 @@ def load_json_file():
 
 
 def save_images(json_data):
+    """Fetches images from twitter, saves them to images/., then modifies the system timestamp to be tweet upload date.
+    If you don't have virtualenv set up or don't want to modify the system file timestamp (your loss!), you can leave
+    the commented out parts as they are.
+    :param json_data: JSON blob produced by running tweet_snippet.js in your browser
+    :return:
+    """
     print('processing ' + str(len(json_data)) + ' files (however, there may be duplicates, so the real # may be less)')
     # json_data = json_data[:4]  # uncomment for testing
     for image in json_data:
@@ -23,9 +30,13 @@ def save_images(json_data):
 
         # update creation time to match the upload time of tweet
         datetime_ts = datetime.datetime.strptime(image['dateTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        epoch_s = datetime_ts.timestamp()
 
-        setctime('images/' + file_name, epoch_s)
+        # Uncomment for Mac
+        os.system('SetFile -d "{}" {}'.format(datetime_ts.strftime('%m/%d/%Y %H:%M:%S'), 'images/' + file_name))
+
+        # Uncomment for Windows
+        # epoch_s = datetime_ts.timestamp()
+        # setctime('images/' + file_name, epoch_s)  # Windows only
 
 
 if __name__ == "__main__":
